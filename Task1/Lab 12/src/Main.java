9f205db0a360bd4bd88f0e157869cec437cb20a9
Ravwvil <rawil.kazeev@gmail.com>
2023-11-17 19:23:07 +0300

    Update Main.java

--- Task1/Lab 12/src/Main.java
+++ Task1/Lab 12/src/Main.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
         String inputFileName = "input.txt";
